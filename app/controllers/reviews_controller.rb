68101c5a7794161e039503598134de758cc308bb
stepholdcorn <stephieoldcorn@gmail.com>
2015-02-19 13:45:06 +0000

    Prevents user leaving a review if they are not signed in

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,5 @@
 class ReviewsController < ApplicationController
+  before_action :authenticate_user!
 
   def new
     @restaurant = Restaurant.find(params[:restaurant_id])
--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -4,30 +4,39 @@ feature 'reviewing' do
   before {Restaurant.create name: 'KFC'}
 
   scenario 'allows user to leave a review using a form' do
-    visit '/restaurants'
-    click_link 'Review KFC'
-    fill_in 'Thoughts', with: 'so so'
-    select '3', from: 'Rating'
-    click_button 'Leave Review'
-
+    sign_up
+    leave_review('so so', 3)
     expect(current_path).to eq '/restaurants'
     expect(page).to have_content('so so')
   end
 
+  scenario 'prevents user leaving a review unless they are signed in' do
+    visit '/restaurants'
+    click_link 'Review KFC'
+    expect(page).to have_content('You need to sign in or sign up before continuing')
+  end
+
   scenario 'displays an average rating for all reviews' do
+    sign_up
     leave_review('So so', '3')
     leave_review('Great', '5')
     expect(page).to have_content('Average rating: 4')
   end
 
-  # Methods
+end
 
-  def leave_review(thoughts, rating)
-    visit '/restaurants'
-    click_link 'Review KFC'
-    fill_in 'Thoughts', with: thoughts
-    select rating, from: 'Rating'
-    click_button 'Leave Review'
-  end
+def leave_review(thoughts, rating)
+  visit '/restaurants'
+  click_link 'Review KFC'
+  fill_in 'Thoughts', with: thoughts
+  select rating, from: 'Rating'
+  click_button 'Leave Review'
+end
 
+def sign_up
+  visit '/users/sign_up'
+  fill_in 'Email', with: 'test@test.com'
+  fill_in 'Password', with: 'testtest'
+  fill_in 'Password confirmation', with: 'testtest'
+  click_button 'Sign up'
 end
